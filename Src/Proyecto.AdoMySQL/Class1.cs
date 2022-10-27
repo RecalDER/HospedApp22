5cb5e7524096ede6011d0d6430098b2d95ef204f
RecalDER <facundorecalde34@gmail.com>
2022-10-26 23:51:56 -0300

    c

--- Src/Proyecto.AdoMySQL/Class1.cs
+++ Src/Proyecto.AdoMySQL/Class1.cs
@@ -23,4 +23,3 @@ public class MapHotel : Mapeador<Hotel>
             
         };
 }
-
--- bd/00ddl.sql
+++ bd/00ddl.sql
@@ -1,72 +1,75 @@
--- Active: 1646654372192@@127.0.0.1@3306@hospedapp
-
 DROP DATABASE IF EXISTS HospedApp;
-
 CREATE DATABASE HospedApp;
-
 USE HospedApp;
 
-CREATE TABLE
-    Hotel(
-        idHotel SMALLINT NOT NULL,
-        nombre VARCHAR(25) NOT NULL,
-        domicilio VARCHAR(25) NOT NULL,
-        email VARCHAR(25) NOT NULL,
-        contrasenia CHAR(64) NOT NULL,
-        estrellas TINYINT UNSIGNED NOT NULL,
-        PRIMARY KEY (idHotel)
-    );
-
-CREATE TABLE Cliente(
-        idCliente MEDIUMINT NOT NULL,
-        nombre VARCHAR(25) NOT NULL,
-        apellido VARCHAR(25) NOT NULL,
-        email VARCHAR(25) NOT NULL,
-        contrasenia CHAR(64) NOT NULL,
-        PRIMARY KEY (idCliente)
-    );
-
+CREATE TABLE Hotel(
+    idHotel SMALLINT NOT NULL,
+    nombre VARCHAR(25) NOT NULL,
+    domicilio VARCHAR(25) NOT NULL,
+    email VARCHAR(25) NOT NULL,
+    contrasenia CHAR(64) NOT NULL,
+    estrellas TINYINT UNSIGNED NOT NULL,
+    PRIMARY KEY (idHotel)
+);
+CREATE TABLE cliente(
+	idCliente MEDIUMINT NOT NULL,
+    nombre VARCHAR(25) NOT NULL,
+    apellido VARCHAR(25) NOT NULL,
+    email VARCHAR(25) NOT NULL,
+    contrasenia CHAR(64) NOT NULL,
+    PRIMARY KEY (idCliente)
+);
 CREATE TABLE Cama(
-        idTipoCama TINYINT NOT NULL,
-        tipoCama VARCHAR(15) NOT NULL,
-        cantPersonas TINYINT NOT NULL,
-        PRIMARY KEY (idTipoCama)
-    );
-
+    idTipoCama TINYINT NOT NULL,
+    tipoCama VARCHAR(15) NOT NULL,
+    cantPersonas TINYINT NOT NULL,
+    PRIMARY KEY (idTipoCama)
+);
 CREATE TABLE Cuarto(
-        numCuarto TINYINT UNSIGNED NOT NULL,
-        idHotel SMALLINT NOT NULL,
-        idCuarto SMALLINT auto_increment NOT NULL,
-        cochera BOOLEAN NOT NULL,
-        costoNoche DECIMAL(7, 2) NOT NULL,
-        descripcion VARCHAR(60) NULL,
-        PRIMARY KEY (idCuarto),
-        CONSTRAINT fk_cuarto_idHotel FOREIGN KEY (idHotel) REFERENCES Hotel (idHotel)
-    );
-
+    numCuarto TINYINT UNSIGNED NOT NULL,
+    idHotel SMALLINT NOT NULL,
+    idCuarto SMALLINT auto_increment NOT NULL,
+    cochera BOOLEAN NOT NULL,
+    costoNoche DECIMAL(7,2) NOT NULL,
+    descripcion VARCHAR(60) NULL,
+    PRIMARY KEY (idCuarto),
+    CONSTRAINT fk_cuarto_idHotel FOREIGN KEY (idHotel)
+		REFERENCES Hotel (idHotel)
+);
 CREATE TABLE Reserva(
-        idReserva INT NOT NULL,
-        idHotel SMALLINT NOT NULL,
-        inicio DATE NOT NULL,
-        fin DATE NOT NULL,
-        idCliente MEDIUMINT NOT NULL,
-        idCuarto SMALLINT NOT NULL,
-        costoNoche DECIMAL(7, 2) NOT NULL,
-        calificacionCliente TINYINT UNSIGNED NULL,
-        calificacionHotel TINYINT UNSIGNED NULL,
-        descripcion VARCHAR(60) NULL,
-        cancelado BOOLEAN DEFAULT FALSE NOT NULL,
-        PRIMARY KEY (idReserva),
-        CONSTRAINT fk_Reserva_idhotel FOREIGN KEY(idHotel) REFERENCES Hotel (idHotel),
-        CONSTRAINT fk_Reserva_idcliente FOREIGN KEY(idCliente) REFERENCES Cliente (idCliente),
-        CONSTRAINT fk_Reserva_idcuarto FOREIGN KEY(idCuarto) REFERENCES Cuarto (idCuarto)
-    );
+    idReserva INT NOT NULL,
+    idHotel SMALLINT NOT NULL,
+    inicio DATE NOT NULL,
+    fin DATE NOT NULL,
+    idCliente MEDIUMINT  NOT NULL,
+    idCuarto SMALLINT auto_increment NOT NULL,
+    costoNoche DECIMAL(7,2) NOT NULL,
+    calificacionCliente TINYINT UNSIGNED NULL,
+    calificacionHotel TINYINT UNSIGNED NULL,
+    descripcion VARCHAR(60) NULL,
+    cancelado BOOLEAN DEFAULT FALSE NOT NULL,
+    PRIMARY KEY (idReserva),
+    CONSTRAINT fk_Reserva_idhotel FOREIGN KEY(idHotel)
+		REFERENCES Hotel (idHotel),
+    CONSTRAINT fk_Reserva_idcliente FOREIGN KEY(idCliente)
+		REFERENCES Cliente (idCliente),
+	CONSTRAINT fk_Reserva_idcuarto FOREIGN KEY(idCuarto)
+		REFERENCES Cuarto (idCuarto)
+);
 
 CREATE TABLE Tcama(
-        idTipoCama TINYINT NOT NULL,
-        idcuarto SMALLINT auto_increment NOT NULL,
-        cantDeCamas TINYINT NOT NULL,
-        PRIMARY KEY (idTipoCama, idCuarto),
-        CONSTRAINT fk_Tcama_idCama FOREIGN KEY (idTipoCama) REFERENCES Cama (idTipoCama),
-        CONSTRAINT fk_Tcama_idcuarto FOREIGN KEY (idCuarto) REFERENCES Cuarto (idCuarto)
-    );
+    idTipoCama TINYINT NOT NULL,
+	idcuarto SMALLINT auto_increment NOT NULL,
+    cantDeCamas TINYINT NOT NULL,
+    PRIMARY KEY (idTipoCama,idCuarto),
+    CONSTRAINT fk_Tcama_idCama FOREIGN KEY (idTipoCama)
+		REFERENCES Cama (idTipoCama),
+	CONSTRAINT fk_Tcama_idcuarto FOREIGN KEY (idCuarto)
+		REFERENCES Cuarto (idCuarto)
+);
+
+
+
+
+
+
